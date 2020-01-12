7395b717b48589b2fd73e9c19c73642f3ff69d34
Philipp Kanne <philipp.kanne@gmail.com>
2020-01-12 12:24:40 +0100

    feature/added-supply-to-statistics

--- src/Homie/Boot/BootNormal.cpp
+++ src/Homie/Boot/BootNormal.cpp
@@ -190,6 +190,11 @@ void BootNormal::loop() {
     Interface::get().getLogger() << F("  • Uptime: ") << uptimeStr << F("s") << endl;
     uint16_t uptimePacketId = Interface::get().getMqttClient().publish(_prefixMqttTopic(PSTR("/$stats/uptime")), 1, true, uptimeStr);
 
+    uint32_t vcc = ESP.getVcc();
+    char vccStr[20 + 1];
+    Interface::get().getLogger() << F("  • VCC: ") << vccStr << endl;
+    uint16_t uptimePacketId = Interface::get().getMqttClient().publish(_prefixMqttTopic(PSTR("/$stats/supply")), 1, true, vccStr);
+
     if (intervalPacketId != 0 && signalPacketId != 0 && uptimePacketId != 0) _statsTimer.tick();
     Interface::get().event.type = HomieEventType::SENDING_STATISTICS;
     Interface::get().eventHandler(Interface::get().event);
